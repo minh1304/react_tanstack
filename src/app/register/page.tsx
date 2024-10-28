82bed592256ea9b465bc5518f35300b95dc61a19
Tuan Minh <vtm1304@gmail.com>
2024-10-28 21:58:59 +0700

    minh.vo

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -307,7 +307,7 @@ export default function Register() {
                                         disabled={!canSubmit}
                                         className="w-full mt-4 inline-flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-bold text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 disabled:bg-gray-400"
                                     >
-                                        {isSubmitting ? '...' : 'Create'}
+                                        {isSubmitting ? '...' : 'Submit'}
                                     </button>
                                 )}
                             />
